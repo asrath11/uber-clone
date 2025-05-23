03dba1db8c7a8b37f504288b283431928f152c8d
Asrath <pasrath.2004@gmail.com>
2025-05-23 18:32:42 +0530

    added home page

---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import MainHero from '../HeroSection/MainHero';
+import '../../App.css';
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <MainHero />
+    </>
+  );
+}
+
+export default Home;
