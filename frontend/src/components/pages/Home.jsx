3f1ea9cd0d4e58d0baaaac9bbb6b99e4afac6888
Asrath <pasrath.2004@gmail.com>
2025-05-24 12:11:14 +0530

    added hero.jsx in home page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import MainHero from '../HeroSection/MainHero';
+import Hero from '../HeroSection/Hero';
+import HERO_DATA from '../HeroSection/data';
 import '../../App.css';
 function Home() {
   return (
     <>
       <NavBar />
       <MainHero />
+      {HERO_DATA.map((hero, index) => (
+        <Hero
+          key={index}
+          title={hero.title}
+          desc={hero.desc}
+          imgSrc={hero.imgSrc}
+          btnText={hero.btnText}
+          imgDirection={index % 2 === 0 ? 'right' : 'left'}
+          btnLink={hero.btnLink}
+        />
+      ))}
     </>
   );
 }
