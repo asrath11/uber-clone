dc69387d3864c34a9b0055310d360a07e735bb0a
Asrath <pasrath.2004@gmail.com>
2025-05-31 11:09:16 +0530

    added logout route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -5,6 +5,8 @@ import {
   registerUser,
   loginUser,
   getUserProfile,
+  updateUserProfile,
+  logoutUser,
 } from '../controllers/auth/user.controller.js';
 import { authUser } from '../middleware/auth.middleware.js';
 
@@ -14,5 +16,7 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', authUser, getUserProfile);
+router.post('/updateUser', authUser, updateUserProfile);
+router.post('/logout', logoutUser);
 
 export default router;
