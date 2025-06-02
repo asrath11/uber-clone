02d3ddc7ba5a4540afb833ab14ecb3f86edfc957
Asrath <pasrath.2004@gmail.com>
2025-06-02 13:39:04 +0530

    added layout and router in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+//import components
+import Layout from './layout.jsx';
+import Home from './components/pages/Home.jsx';
+import SignUp from './components/pages/Auth/User/Signup.jsx';
+import Login from './components/pages/Auth/User/Login.jsx';
+import { UserProvider } from './components/contexts/UserProvider.jsx';
+import Drive from './components/pages/Drive.jsx';
+
+//router
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <>
+      <Route path='/' element={<Layout />}>
+        <Route path='/drive' element={<Drive />} />
+        <Route path='/' element={<Home />} />
+      </Route>
+      <Route path='/auth/user/signup' element={<SignUp />} />
+      <Route path='/auth/user/login' element={<Login />} />
+    </>
+  )
+);
 
 function App() {
-  return <div className='bg-slate-700'>App</div>;
+  return (
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  );
 }
 
 export default App;
