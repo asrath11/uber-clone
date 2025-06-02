13ed70bb4d95693c5ae93295e3771456afe96869
Asrath <pasrath.2004@gmail.com>
2025-06-02 13:39:48 +0530

    added variable for accessing path

---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+// src/config.js
+export const API_URL = import.meta.env.VITE_API_URL;
+export const USER_SIGNUP_URL = import.meta.env.VITE_USER_SIGNUP_URL;
+export const USER_LOGIN_URL = import.meta.env.VITE_USER_LOGIN_URL;
+export const DRIVER_SIGNUP_URL = import.meta.env.VITE_DRIVER_SIGNUP_URL;
+export const DRIVER_LOGIN_URL = import.meta.env.VITE_DRIVER_LOGIN_URL;
