95afb5c259a59cccffa66e88dec66b2a0a625b55
Asrath <pasrath.2004@gmail.com>
2025-06-02 13:41:21 +0530

    added layout for common components'

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+function Layout() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+    </>
+  );
+}
+
+export default Layout;
