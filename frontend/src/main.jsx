2eb31c8f704db4189c21bc5de5ffdbd0b212539a
Asrath <pasrath.2004@gmail.com>
2025-06-02 13:42:07 +0530

    replaced router to app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  createRoutesFromChildren,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
 import './index.css';
-import Home from './components/pages/Home.jsx';
-import Auth from './components/pages/Auth.jsx';
-
-const router = createBrowserRouter(
-  createRoutesFromChildren(
-    <>
-      <Route path='/' element={<Home />} />
-      <Route path='/register' element={<Auth />} />
-    </>
-  )
-);
+import App from './App.jsx';
+// import Login from
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App></App>
   </StrictMode>
 );
